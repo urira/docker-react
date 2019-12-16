21a0694da68326fa794e8ca81939c8efae93887a
varvar <michaels@websplanet.com>
2019-12-16 23:35:51 +0200

    fix test for Update homepage

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/docker compose/i);
+  const linkElement = getByText(/elasticbeanstalk/i);
   expect(linkElement).toBeInTheDocument();
 });
