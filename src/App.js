c0eb8bdb75b84478c0d2a1518f846a0e4eef92f8
varvar <michaels@websplanet.com>
2019-12-16 21:59:02 +0200

    additional files

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi There. Uri I am running with docker compose!!!
         </a>
       </header>
     </div>
